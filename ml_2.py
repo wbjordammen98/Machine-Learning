d25bde83baf57e85a41c12a0463215773a18b732
wbjordammen98 <wbjordammen@gmail.com>
2020-11-09 17:58:38 -0600

    final code push

--- ml_2.py
+++ ml_2.py
@@ -1,22 +1,17 @@
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
 
 nyc = pd.read_csv("ave_hi_nyc_jan_1895-2018.csv")
 
-#print(nyc.head(3))
-#print(nyc.Date.values)
-#print(nyc.Date.values.reshape(-1,1))
-
 x_train, x_test, y_train, y_test = train_test_split(nyc.Date.values.reshape(-1,1), nyc.Temperature.values, random_state=11)
-
-from sklearn.linear_model import LinearRegression
 
 lr = LinearRegression()
 lr.fit(X=x_train, y=y_train)
-
-#print(lr.coef_)
-#print(lr.intercept_)
 
 predicted = lr.predict(x_test)
 expected = y_test
@@ -26,12 +21,6 @@ for p,e in zip(predicted[::5], expected[::5]):
 
 predict = (lambda x: lr.coef_ * x + lr.intercept_)
 
-#print(predict(2020))
-#print(predict(1890))
-#print(predict(2021))
-
-import seaborn as sns
-
 axes = sns.scatterplot(data=nyc,
                        x='Date',
                        y='Temperature',
@@ -40,37 +29,23 @@ axes = sns.scatterplot(data=nyc,
                        legend=False)
 axes.set_ylim(10,70)
 
-import numpy as np
-
 x = np.array([min(nyc.Date.values), max(nyc.Date.values)])
-
-#print(x)
-
 y = predict(x)
 
-#print(y)
-
-import matplotlib.pyplot as plt
-
 line = plt.plot(x,y)
-f1 = plt.figure(1)
 
-####
-import csv
-import pandas as pd
-from sklearn.model_selection import train_test_split
+#### Machine Learning Exercise question 2.
 
 n = csv.reader(open("ave_yearly_temp_nyc_1895-2017.csv"))
 next(n)
 
 lines = list(n)
-#print(lines)
 
 for line in lines:
     date = line[0]
     new_date = date[:-2]
     line[0] = new_date
-###
+
 print(lines)
 
 fields = ['Date','Value','Anomaly']
@@ -80,18 +55,12 @@ with open('ave_yearly_fixed_nyc_1895-2017.csv','w') as f:
     write.writerow(fields)
     write.writerows(lines)
 
-
 nyc_yearly = pd.read_csv("ave_yearly_fixed_nyc_1895-2017.csv")
 
 x_train_yr, x_test_yr, y_train_yr, y_test_yr = train_test_split(nyc_yearly.Date.values.reshape(-1,1), nyc_yearly.Value.values, random_state=11)
 
-from sklearn.linear_model import LinearRegression
-
 lr_yrly = LinearRegression()
 lr_yrly.fit(X=x_train_yr, y=y_train_yr)
-
-#print(lr_yrly.coef_)
-#print(lr_yrly.intercept_)
 
 predicted_yr = lr_yrly.predict(x_test_yr)
 expected_yr = y_test_yr
@@ -101,28 +70,13 @@ for p,e in zip(predicted_yr[::5], expected_yr[::5]):
 
 predict_yr = (lambda x: lr_yrly.coef_ * x + lr_yrly.intercept_)
 
-#print(predict_yr(2020))
-#print(predict_yr(1890))
-#print(predict_yr(2021))
-
-import seaborn as sns
-
 axes = sns.scatterplot(data=nyc_yearly,x='Date',y='Value',hue='Value',palette='winter',legend=False)
 axes.set_ylim(10,70)
 
-import numpy as np
-
 x_yrly = np.array([min(nyc_yearly.Date.values), max(nyc_yearly.Date.values)])
-
-#print(x_yrly)
-
 y_yrly = predict_yr(x_yrly)
 
-#print(y_yrly)
-
-import matplotlib.pyplot as plt
-
 line_yrly = plt.plot(x_yrly,y_yrly)
-#f2 = plt.figure(2)
 
+plt.title("Avg Yearly NYC Temps vs. Avg Jan High NYC Temps")
 plt.show()
--- ml_3.py
+++ ml_3.py
@@ -3,8 +3,8 @@ from sklearn.datasets import fetch_california_housing
 cali = fetch_california_housing()
 
 #print(cali.DESCR)
-print(cali.data.shape)
-print(cali.target.shape)
+#print(cali.data.shape)
+print(cali.target_names)
 print(cali.feature_names)
 
 import pandas as pd
@@ -14,7 +14,7 @@ pd.set_option("max_columns",9)
 pd.set_option("display.width",None)
 
 cali_df = pd.DataFrame(cali.data,columns=cali.feature_names)
-cali_df["MedHouseValue"] = pd.Series(cali.target)
+cali_df["MedHouseVal"] = pd.Series(cali.target)
 
 print(cali_df.head())
 
@@ -23,7 +23,7 @@ sample_df = cali_df.sample(frac=0.1, random_state=17)
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set(font_scale=2)
+sns.set(font_scale=1)
 sns.set_style("whitegrid")
 
 grid = sns.pairplot(
